0d6afa861662c520b3190c20b51cf696c36f2a81
Glitch (pebble-scowl) <none>
2019-01-13 16:55:34 +0000

    🍠🚘 Checkpoint ./routes/api.js:2521121/169 ./package.json:2521121/1802

--- routes/api.js
+++ routes/api.js
@@ -10,12 +10,16 @@
 
 const expect   = require('chai').expect;
 const bcrypt   = require('bcrypt');
-
-const 
+const mongoose = require('mongoose');
 
 module.exports = function (app) {
+  mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+      .get()
+      .post()
+      .put()
+      .delete();
     
   app.route('/api/replies/:board');
 
