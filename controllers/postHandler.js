77c03b255e589051128da66238127a5a233214c4
Glitch (pebble-scowl) <none>
2019-01-13 18:32:34 +0000

    🚩📷 Checkpoint ./routes/api.js:2521121/1578 ./controllers/postHandler.js:2521121/170

---
+++ controllers/postHandler.js
@@ -0,0 +1,10 @@
+const mongodb = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
+const url = process.env.DB_URI;
+
+function PostHandler(){
+  
+
+}
+
+module.exports = P
--- routes/api.js
+++ routes/api.js
@@ -9,47 +9,16 @@
 'use strict';
 
 const expect   = require('chai').expect;
-const bcrypt   = require('bcrypt');
-const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
-  board: {type: String, required: true},
-  text: {type: String, required: true},
-  created_on: {type: Date, required: true, default: new Date()},
-  bumped_on: {type: Date, required: true, default: new Date()},
-  reported: {type: Boolean, required: true, default: false},
-  delete_password: {type: String, required: true},
-  replies: {type: [], default: []},
-})
-
-const Post = mongoose.model('Post', postSchema);
+const PostHandler = require('../controllers/postHandler.js');
 
 module.exports = function (app) {
-  mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
   
   app.route('/api/threads/:board')
       .get(function(req,res){
-        Post.find({board: req.params.board}).select('-_id -reported -delete_password -__v').exec((err,posts)=>{
-          if (err) res.send(err);
-          
-        })
+        
       })
-  
       .post(function(req, res){
-        var hashedPw = bcrypt.hashSync(req.body.delete_password, 12);
-        let newPost = new Post({
-          board: req.params.board,
-          text: req.body.text,
-          created_on: new Date(),
-          bumped_on: new Date(),
-          reported: false,
-          delete_password: hashedPw,
-          replies: []
-        })
-        newPost.save(err=>{
-          if (err) res.json({error: err})
-        })
-        res.redirect('/b/'+req.params.board)
+        
       })
   
       .put()
