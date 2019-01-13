23a6e966f106a913e8631810d0e2acb9a3c9d70b
Glitch (pebble-scowl) <none>
2019-01-13 18:43:34 +0000

    🐦⛹ Checkpoint ./controllers/postHandler.js:2521121/704

--- controllers/postHandler.js
+++ controllers/postHandler.js
@@ -3,8 +3,25 @@ const ObjectID = require('mongodb').ObjectID;
 const url = process.env.DB_URI;
 
 function PostHandler(){
-  
+  this.postList = function(req,res){
+    let board = req.params.board;
+    mongodb.connect(url, (err,db)=>{
+      db.collection(board)
+        .find({},{
+          reported: 0,
+          delete_password: 0
+        })
+        .sort({bumped_on: -1})
+        .limit(10)
+        .toArray((err,posts)=>{
+          if (err) res.json({error: err})
+          posts.map(p=>{
+            
+          })
+        })
+    })
+  }
 
 }
 
-module.exports = P
+module.exports = PostHandler;
